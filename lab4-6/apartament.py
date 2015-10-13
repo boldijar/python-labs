8674d2b79be527b9d45e82360e8647f7c67e7a40
Boldijar Paul <paul.bv96@yahoo.com>
2015-10-13 11:13:20 +0300

    added apartaments methods

--- lab4-6/apartament.py
+++ lab4-6/apartament.py
@@ -9,5 +9,26 @@ class Apartament:
     def __init__(self,number):
         self.bills = []
         self.number = number
-        
+
+class Apartaments:
+
+    def __init__(self):
+        self.all = []
+        for i in range (0,100):
+            apartament = Apartament(i)
+            self.all.append(apartament)
+
+    def getApartamentCost(self,apartamentIndex):
+        cost = 0
+        apartament = self.all[apartamentIndex]
+        for i in range (0,len(apartament.bills)):
+            cost = cost + apartament.bills[i].cost
+        return cost
+
+    def getApartamentsCount(self):
+        return len(self.all)
+
+apartaments = Apartaments()
+
+print apartaments.getApartamentCost(2)
       
