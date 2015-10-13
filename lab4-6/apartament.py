f69f8933c4c551aead6cdb2bd3fd462a964ef89e
Boldijar Paul <paul.bv96@yahoo.com>
2015-10-13 10:33:21 +0300

    added number for apartament

--- lab4-6/apartament.py
+++ lab4-6/apartament.py
@@ -2,7 +2,8 @@ from apartament_type import ApartamentType
 
 class Apartament:
 
-    def __init__(self, type, cost):
+    def __init__(self, type, cost, number):
         self.type = type
         self.cost = cost
+        self.number = number
       
