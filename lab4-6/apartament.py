0dd8a838ecaba7d1880cacf8a239966034530c03
Boldijar Paul <paul.bv96@yahoo.com>
2015-10-18 14:17:55 +0300

    added new methods & main

--- lab4-6/apartament.py
+++ lab4-6/apartament.py
@@ -10,17 +10,24 @@ class Apartament:
         self.bills = []
         self.number = number
 
+    def addBill(self,bill):
+        self.bills.append(bill)
+
 class Apartaments:
 
     def __init__(self,all):
         self.all = all
-        
+
     def __init__(self):
         self.all = []
         for i in range (0,100):
             apartament = Apartament(i)
             self.all.append(apartament)
 
+
+    def get(self,index):
+        return self.all[index]
+    
     def getApartamentCost(self,apartamentIndex):
         cost = 0
         apartament = self.all[apartamentIndex]
@@ -38,6 +45,20 @@ class Apartaments:
     
     def getApartamentsCount(self):
         return len(self.all)
+
+    def getCertainBillsFromAllApartaments(self,billType):
+        list = []
+        for i in range (0,len(self.all)):
+            apartament= self.all[i]
+            for j in range (0,len(apartament.bills)):
+                bill = apartament.bills[j];
+                if bill.type == billType:
+                    list.append(bill)
+    def getBillsCost(self,bills):
+        cost = 0
+        for i in range (0,len(bills)):
+            cost = cost + bills[i].cost
+        return cost
 
     def clearApartamentBills(self,apartamentIndex):
         self.all[apartamentIndex].bills = []
--- lab4-6/main.py
+++ lab4-6/main.py
@@ -1,8 +1,14 @@
 from bill import Bill
 from bill import BillMethods
 from bill_type import BillType
+from apartament import Apartament
+from apartament import Apartaments
 
+apartaments = Apartaments()
 
+def addBill(apartamentNumber,billType,cost):
+    bill = Bill(billType,cost)
+    apartaments.get(apartamentNumber).addBill(bill)
 
 
 
